0c3b9a990de1efc72c5b350afb27fd7e47cfed23
phit <phit@hush.com>
2017-08-06 00:11:49 +0200

    created

---
+++ README.md
@@ -0,0 +1,11 @@
+# patreon-to-discord-webhooks
+
+Patreon can send you events when someone adds, updates or deletes their pledge to you, I couldn't find anything that can process those. So here's a quick PHP script.
+
+## Setup
+
+1. download script
+2. put it somewhere accessible from the web
+3. edit script and add your Discord webbhook URL at the top
+4. create a [https://www.patreon.com/platform/documentation/clients](Patreon App) 
+5. enable all three Patreon webhooks [https://www.patreon.com/platform/documentation/webhooks](here) and point them at the script location
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+$discord_webhook = "YOURLINKHERE https://discordapp.com/api/webhooks/...."
+
+// post to discord snippet from https://www.reddit.com/r/discordapp/comments/58hry5/simple_php_function_for_posting_to_webhook/
+function postToDiscord($message)
+{
+    $data = array("content" => $message, "username" => "Patreon Bot");
+    $curl = curl_init($discord_webhook);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    return curl_exec($curl);
+}
+
+
+// this saves the post data you get on your endpoint
+$data = @file_get_contents('php://input');
+// decode json post data to arrays
+$event_data = json_decode($data, true);
+
+// also get the headers patreon sends
+$event = $_SERVER['HTTP_X_PATREON_EVENT']; // pledges:create pledges:delete pledges:update
+$sig = $_SERVER['HTTP_X_PATREON_SIGNATURE']; // you should probably check those, oh well
+
+// get all the user info
+$amount_cents = $event_data['data']['attributes']['amount_cents'];
+$patron_id = $event_data['data']['relationships']['patron']['data']['id'];
+
+foreach ($event_data['included'] as $included_data) {
+    if ($included_data['type'] == 'user' && $included_data['id'] == $patron_id) {
+        $user_data = $included_data;
+    }
+}
+
+$patron_url = $user_data['attributes']['url'];
+$patron_fullname = $user_data['attributes']['full_name'];
+
+
+// send event to discord
+if ($event == "pledges:create") {
+    postToDiscord(":star: " . $patron_fullname . " just pledged for " . $amount_cents . " cents! <" . $patron_url . ">");
+} else if ($event == "pledges:delete") {
+    postToDiscord(":disappointed: " . $patron_fullname . " just removed their pledge! <" . $patron_url . ">");
+} else if ($event == "pledges:update") {
+    postToDiscord(":open_mouth: " . $patron_fullname . " just updated their pledge to " . $amount_cents . " cents! <" . $patron_url . ">");
+} else {
+    postToDiscord($event . ": something happened with patreon that phit forgot to implement");
+}
+
+?>
